e4ac16548fb82bd742286471b78e6e37adcb3aba
Tanzia Rahman <163232597+tanziarahman@users.noreply.github.com>
2024-11-03 10:15:09 +0600

    Delete src directory temporarily

--- src/CategoryExistsException.java
+++
@@ -1,5 +0,0 @@
-public class CategoryExistsException extends RuntimeException{
-    public CategoryExistsException(String message){
-        super(message);
-    }
-}
--- src/SubCategoryExistsException.java
+++
@@ -1,5 +0,0 @@
-public class SubCategoryExistsException extends RuntimeException{
-    public SubCategoryExistsException(String message){
-        super(message);
-    }
-}
