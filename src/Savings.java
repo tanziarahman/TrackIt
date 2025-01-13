266aeb7c6b07b61eb6f425ba2aced9e8453f55ec
tanziarahman <tanziarahman566@gmail.com>
2025-01-13 19:35:41 +0600

    Savings class added

---
+++ src/Savings.java
@@ -0,0 +1,14 @@
+public class Savings {
+    private String category;
+    private double amount;
+    public Savings(String category, double amount){
+        this.category = category;
+        this.amount = amount;
+    }
+    public  String getCategory(){
+        return category;
+    }
+    public double getAmount(){
+        return  amount;
+    }
+}
