1809c22decc0cdc2631b24e2ca238f50e9a8aed6
Sayesha140 <enaya871@gmail.com>
2025-03-09 02:28:54 +0600

    Budget manager updated.

--- src/BudgetManager.java
+++ src/BudgetManager.java
@@ -1,7 +1,5 @@
-import java.io.*;
 import java.time.Month;
 import java.time.Year;
-import java.util.HashMap;
 import java.util.Map;
 
 public class BudgetManager {
@@ -9,17 +7,18 @@ public class BudgetManager {
     CategoryManager categoryManager;
     private Map<String, Budget> budgets;
     private double monthlyIncome;
+    private IBudgetStorage storage;
     private Month month;
     private Year year;
 
 
-    public BudgetManager(CategoryManager categoryManager,Month month,Year year){
+    public BudgetManager(CategoryManager categoryManager,Month month,Year year,IBudgetStorage storage){
         this.categoryManager = categoryManager;
         this.month=month;
         this.year=year;
-        budgets = new HashMap<>();
-        monthlyIncome = 0.0;
-        loadMonthBudgetData();
+        this.storage=storage;
+        this.monthlyIncome= storage.loadMonthlyIncome(month,year);
+        this.budgets=storage.loadMonthBudgetData(month,year);
     }
 
     public  Map<String,Budget> getBudgets(){
@@ -32,13 +31,13 @@ public class BudgetManager {
 
     public void SetMonthlyIncome( double monthlyIncome) {
         this.monthlyIncome = monthlyIncome;
-        saveBudgetMonthDataToCSV();
+        storage.saveBudgetMonthDataToCSV(budgets,monthlyIncome,month,year);
     }
 
 
     public void addToIncome(double additionalIncome) {
         monthlyIncome += additionalIncome;
-        saveBudgetMonthDataToCSV();
+        storage.saveBudgetMonthDataToCSV(budgets,monthlyIncome,month,year);
     }
 
     public double getMonthlyIncome() {
@@ -58,7 +57,7 @@ public class BudgetManager {
             throw new BudgetExistsException("Budget is already set for category " + category.toLowerCase() + ". If you want, you can edit it.");
         }
 
-        saveBudgetMonthDataToCSV();
+        storage.saveBudgetMonthDataToCSV(budgets,monthlyIncome,month,year);
     }
 
     public void deleteCategoryBudget(String category) throws BudgetNotFoundException {
@@ -70,7 +69,7 @@ public class BudgetManager {
             throw new BudgetNotFoundException("No budget found for Category " + category.toLowerCase());
         }
 
-        saveBudgetMonthDataToCSV();
+        storage.saveBudgetMonthDataToCSV(budgets,monthlyIncome,month,year);
     }
 
     public void editCategoryBudget(String category, double newAmount) throws BudgetNotFoundException{
@@ -83,55 +82,9 @@ public class BudgetManager {
             throw new BudgetNotFoundException("No budget found for Category " + category.toLowerCase());
         }
 
-        saveBudgetMonthDataToCSV();
+        storage.saveBudgetMonthDataToCSV(budgets,monthlyIncome,month,year);
     }
 
-
-    public void loadMonthBudgetData() {
-
-        budgets.clear();
-        monthlyIncome = 0.0;
-
-
-        String fileName = month.name().toLowerCase() +year.getValue() +  "Budget.csv";
-        File file = new File(fileName);
-
-        if (!file.exists()) {
-            return;
-        }
-
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                String[] data = line.split(",");
-                if (data[0].equalsIgnoreCase("Income")) {
-                    this.monthlyIncome = Double.parseDouble(data[1]);
-                } else {
-                    budgets.put(data[0], new Budget(data[0], Double.parseDouble(data[1])));
-                }
-            }
-        } catch (IOException e) {
-            System.out.println("Error reading data for " + month.name().toLowerCase());
-        }
-    }
-
-
-    public void saveBudgetMonthDataToCSV() {
-
-        String fileName = month.name().toLowerCase() +year.getValue() + "Budget.csv";
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-            writer.write("Income," + this.monthlyIncome);
-            writer.newLine();
-            for (Budget budget : budgets.values()) {
-                writer.write(budget.getCategory().toLowerCase() + "," + budget.getAmount());
-                writer.newLine();
-            }
-
-        } catch (IOException e) {
-            System.out.println("Error saving data for " + month.name().toLowerCase());
-        }
-    }
 
 
     public void showAllBudgets() {
@@ -141,15 +94,15 @@ public class BudgetManager {
         for (Map.Entry<String, Budget> entry : budgets.entrySet()) {
             System.out.println(index + ". " +StringFormatter.capitalizeFirstLetter(entry.getKey()) + ": " + entry.getValue().getAmount());
             index++;
-
         }
+
     }
 
 
     public void showMonthlyIncome(){
+
         System.out.println("Income for " + StringFormatter.capitalizeFirstLetter(month.name()) + " " +year+ " : " + this.monthlyIncome);
     }
-
 
     public double getBudgetAmountForCategory(String category) throws BudgetNotFoundException{
 
@@ -162,7 +115,6 @@ public class BudgetManager {
             throw new BudgetNotFoundException("No budget found for category " +category.toLowerCase());
         }
     }
-
 
     public double getTotalBudget(){
         return budgets.values().stream().mapToDouble(Budget::getAmount).sum();
@@ -180,6 +132,5 @@ public class BudgetManager {
     public boolean budgetExists(String category){
         return budgets.containsKey(category.toLowerCase());
     }
-
 
 }
