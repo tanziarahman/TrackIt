a34448f7ddaf2f320703634218352865ec04b1e1
Jarin001 <snehasenmoni.5october@gmail.com>
2025-03-16 01:46:57 +0600

    Transaction manager interface changed

--- src/TransactionManagerInterface.java
+++ src/TransactionManagerInterface.java
@@ -1,11 +1,11 @@
 import java.io.IOException;
 import java.util.Date;
 
-public interface TransactionManagerInterface {
-    Transaction addTransaction(double amount, String category, String subCategory, Date date, String description) throws IOException;
-    void editTransaction(int transactionID, double newAmount, String newCategory, String newSubCategory, Date newDate, String newDescription) throws IOException, TransactionNotFoundException;
-    void deleteTransaction(int transactionID) throws IOException, TransactionNotFoundException;
-    String getTransactionsSummary();
-    boolean transactionExists(int transactionID);
-}
+public interface TransactionManagerInterface{
+        Transaction addTransaction(double amount, String category, String subCategory, Date date, String description) throws IOException, BudgetNotFoundException;
+        void editTransaction(int transactionID, double newAmount, String newCategory, String newSubCategory, Date newDate, String newDescription) throws IOException, TransactionNotFoundException, BudgetNotFoundException;
+        void deleteTransaction(int transactionID) throws IOException, TransactionNotFoundException;
+        String getTransactionsSummary();
+        boolean transactionExists(int transactionID);
+        }
 
